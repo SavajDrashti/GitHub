410b81d50c68c35318052d84853e04786d8cc82d
SavajDrashti <savajdrashti2511@gmail.com>
2024-07-07 19:02:29 +0530

    integration of github api

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import { Route, Routes } from "react-router-dom"
+import { Toaster } from 'react-hot-toast'
+
 import LoginPage from './pages/LoginPage'
 import SignUpPage from './pages/SignUpPage'
 import HomePage from './pages/HomePage'
 import Sidebar from './components/Sidebar'
 import ExplorePage from './pages/ExplorePage'
 import LikesPage from './pages/LikesPage'
+
 
 
 function App() {
@@ -21,6 +24,7 @@ function App() {
           <Route path="/explore" element={<ExplorePage/>}/>
           <Route path="/likes" element={<LikesPage/>}/>
         </Routes>
+        <Toaster/>
       </div>
     </div>
   )
--- frontend/src/components/SortRepos.jsx
+++ frontend/src/components/SortRepos.jsx
@@ -1,27 +1,26 @@
 import React from 'react'
 
-const SortRepos = () => {
+const SortRepos = ({onSort, sortType}) => {
+
+	const BUTTONS = [
+		{type: "recent", text: "Most Recent"},
+		{type: "stars", text: "Most Stars"},
+		{type: "forks" , text: "Most Forks"},
+	];
   return (
     
-         <div className='mt-0 flex justify-center lg:ms-96'>
+        <div className='mb- mt-2 flex w-full justify-center'>
+		{BUTTONS.map((button) => (
 			<button
-				type='button'
-				className={`py-2.5 px-5 me-2 mb-2 text-xs sm:text-sm font-medium focus:outline-none rounded-lg bg-glass`}
+			key={button.type}
+			type="button"
+			className={`py-2.5 px-5 me-2 mb-2  text-xs sm:text-sm font-medium focus:outline-none rounded-lg bg-glass
+				${button.type == sortType ? "border-blue-500" : ""}`}
+				onClick={() => onSort(button.type)}
 			>
-				Most Recent
+				{button.text}
 			</button>
-			<button
-				type='button'
-				className={`py-2.5 px-5 me-2 mb-2  text-xs sm:text-sm font-medium focus:outline-none rounded-lg bg-glass`}
-			>
-				Most Stars
-			</button>
-			<button
-				type='button'
-				className={`py-2.5 px-5 me-2 mb-2  text-xs sm:text-sm font-medium focus:outline-none rounded-lg bg-glass`}
-			>
-				Most Forks
-			</button>
+		))}		 
 		</div>
      
     
